bf0690c9de8c11510da34774a2b690b8872d73bc
HeoJongWoon <whddns0902@gmail.com>
2023-09-28 21:16:02 +0900

    ssr, csr방식의 이해

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 function App() {
-	return (
-		<h1>Hello</h1>
-	);
+	return <h1>Hello</h1>;
 }
 
 export default App;
+
+/*
+	SSR vs CSR
+
+	SSR - Server Side Rendering
+
+	CSR - Client Side Rendering
+*/
